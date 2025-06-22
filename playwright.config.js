edf64b3af140043cbcf2c79e2bfd88cc21bdda98
Christopher Gantt <gantt.art@gmail.com>
2025-06-22 18:38:35 -0400

    Fixed e2e testing errors from github actions

--- playwright.config.js
+++ playwright.config.js
@@ -22,8 +22,8 @@ module.exports = defineConfig({
     outputDir: 'tests/test-output/test-results',
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
     use: {
-    /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
+        /* Base URL to use in actions like `await page.goto('/')`. */
+        // baseURL: 'http://127.0.0.1:3000',
 
         /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
         trace: 'on-first-retry',
@@ -40,16 +40,6 @@ module.exports = defineConfig({
         {
             name: 'chromium',
             use: { ...devices['Desktop Chrome'] }
-        },
-
-        {
-            name: 'firefox',
-            use: { ...devices['Desktop Firefox'] }
-        },
-
-        {
-            name: 'webkit',
-            use: { ...devices['Desktop Safari'] }
         }
 
         /* Test against mobile viewports. */
@@ -62,15 +52,15 @@ module.exports = defineConfig({
         //   use: { ...devices['iPhone 12'] },
         // },
 
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
+        /* Test against branded browsers. */
+        // {
+        //   name: 'Microsoft Edge',
+        //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
+        // },
+        // {
+        //   name: 'Google Chrome',
+        //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
+        // },
     ]
 
     /* Run your local dev server before starting the tests */
